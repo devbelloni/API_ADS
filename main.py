2f56abc48bc7c0bc6b8bf8fc5dfe71493c8ae186
Marcio Belloni <83551398+devbelloni@users.noreply.github.com>
2025-05-19 22:25:26 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI(title="API Simples")
+
+class TodoCreate(BaseModel):
+    title: str
+    completed: bool = False
+
+class TodoResponse(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
+todos=[]
+
+@app.post("/v1/todos", response_model=TodoResponse, status_code=201)
+async def create_todo(todo: TodoCreate):
+    todo_id= len(todos)+1
+    new_todo = {
+        "id": todo_id,
+        "title": todo.title,
+        "completed": todo.completed
+    }
+    todos.append(new_todo)
+    return new_todo
+
+@app.get("v1/todos", response_model=List[TodoResponse])
+async def get_todos():
+    return todos
+
+        
